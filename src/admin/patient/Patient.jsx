491310902e20fcac7921e6d6bddb86848a8775d8
Flowernoza <cadillac2111@Salmoorbeks-MacBook-Pro.local>
2023-11-01 22:22:13 +0500

    filtered

--- src/admin/patient/Patient.jsx
+++ src/admin/patient/Patient.jsx
@@ -1,14 +1,29 @@
 import "./Patient.css";
 import { BiSearch, BiSolidTrashAlt } from "react-icons/bi";
-import {FaUserAlt} from 'react-icons/fa'
+import { FaUserAlt } from "react-icons/fa";
 import { patients } from "../../static/PatientList";
 import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function Patient() {
+  document.title = "Sadaf ⏤ bemorlar ro'yxati";
   const [patient, Setpatient] = useState(patients);
   const [name, SetName] = useState("");
 
-  function deletePatient(id) {
-    Setpatient(patient.filter((patient) => patient.id !== id));
+  function filterData(e) {
+    const value = e.target.value;
+    if (value === "30 yoshdan katta") {
+      return Setpatient(patients.filter((patient) => patient.age > 30));
+    } else if (value === "1 yoshdan 15 gacha") {
+      return Setpatient(patients.filter((patient) => patient.age <= 15));
+    } else if (value === "15 yoshdan 30 gacha") {
+      return Setpatient(
+        patients.filter((patient) => patient.age > 15 && patient.age <= 30)
+      );
+    } else {
+      return Setpatient(patients);
+    }
   }
 
   let sortedData = patients.sort((a, b) => {
@@ -31,53 +46,74 @@ function Patient() {
       );
     }
   }, [name]);
-
+  function deletePatient(id) {
+    const alerting = window.confirm("Do you want to delete this client?");
+    if (alerting) {
+      Setpatient(patient.filter((patient) => patient.id !== id));
+      toast.success("deleted", {
+        position: "top-right",
+        autoClose: 1500,
+        hideProgressBar: true,
+      });
+    }
+  }
   return (
     <div className="Patient">
-      <div className="searchbar">
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => {
-            SetName(e.target.value);
-          }}
-        />
-        <BiSearch />
+      <h1>Bemorlar Ro'yxati</h1>
+      <ToastContainer />
+      <div className="patient__header">
+        <div className="patient__searchbar">
+          <select className="patient__select" onChange={filterData}>
+            <option>Barchasi</option>
+            <option>1 yoshdan 15 gacha</option>
+            <option>15 yoshdan 30 gacha</option>
+            <option>30 yoshdan katta</option>
+          </select>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => {
+              SetName(e.target.value);
+            }}
+          />
+          <BiSearch />
+        </div>
       </div>
-      {patient.length ? 
-      <table>
-        <caption>Patients List</caption>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Age</th>
-            <th>Remove</th>
-          </tr>
-        </thead>
-        <tbody>
-          {patient.map((item, index) => (
-            <tr key={index}>
-              <th className="number__patient">{index + 1}</th>
-              <td>{item.firstname}</td>
-              <td>{item.lastname}</td>
-              <td>{item.age}</td>
-              <td className="delete__patient">
-                <BiSolidTrashAlt
-                  onClick={() => deletePatient(item.id)}
-                  className="trashban"
-                />
-              </td>
+
+      {patient.length ? (
+        <table>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Ismi</th>
+              <th>Familiyasi</th>
+              <th>Yoshi</th>
+              <th>O'chirish</th>
             </tr>
-          ))}
-        </tbody>
+          </thead>
+          <tbody>
+            {patient.map((item, index) => (
+              <tr key={index}>
+                <th className="number__patient">{index + 1}</th>
+                <td>{item.firstname}</td>
+                <td>{item.lastname}</td>
+                <td>{item.age}</td>
+                <td className="delete__patient">
+                  <BiSolidTrashAlt
+                    onClick={() => deletePatient(item.id)}
+                    className="trashban"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-        : <div className="empty__patient">
-          <FaUserAlt/>
-        <h2>No Patients Found</h2>
-        </div> 
-      }
+      ) : (
+        <div className="empty__patient">
+          <FaUserAlt />
+          <h2>No Patients Found</h2>
+        </div>
+      )}
     </div>
   );
 }
--- src/static/PatientList.js
+++ src/static/PatientList.js
@@ -1,9 +1,9 @@
 export const patients = [
   { id: 1, firstname: "Gulnoza", lastname: "Tursunova", age: 20 },
-  { id: 2, firstname: "Mastura", lastname: "Odilova", age: 20 },
-  { id: 3, firstname: "Madina", lastname: "Rahimova", age: 20 },
-  { id: 4, firstname: "Aziza", lastname: "Olimova", age: 20 },
-  { id: 5, firstname: "Diyora", lastname: "Hakimova", age: 20 },
-  { id: 6, firstname: "Sabina", lastname: "Ismoilova", age: 20 },
-  { id: 7, firstname: "Komila", lastname: "Saidova", age: 20 },
+  { id: 2, firstname: "Mastura", lastname: "Odilova", age: 10 },
+  { id: 3, firstname: "Madina", lastname: "Rahimova", age: 5 },
+  { id: 4, firstname: "Aziza", lastname: "Olimova", age: 27 },
+  { id: 5, firstname: "Diyora", lastname: "Hakimova", age: 30 },
+  { id: 6, firstname: "Sabina", lastname: "Ismoilova", age: 36 },
+  { id: 7, firstname: "Komila", lastname: "Saidova", age: 40 },
 ];
