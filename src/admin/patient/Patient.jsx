9c184ea544c18b45a7b238ce6d049ff1eeec2f06
Flowernoza <cadillac2111@Salmoorbeks-MacBook-Pro.local>
2023-11-01 17:28:51 +0500

    search patient

--- src/admin/patient/Patient.jsx
+++ src/admin/patient/Patient.jsx
@@ -1,37 +1,85 @@
 import "./Patient.css";
-import {BiSolidTrashAlt} from 'react-icons/bi'
+import { BiSearch, BiSolidTrashAlt } from "react-icons/bi";
+import {FaUserAlt} from 'react-icons/fa'
 import { patients } from "../../static/PatientList";
+import { useEffect, useState } from "react";
 function Patient() {
-    return (
-      <div className="Patient">
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Firstname</th>
-              <th>Lastname</th>
-              <th>Age</th>
-              <th>Remove</th>
-            </tr>
-          </thead>
-          <tbody>
-            {patients.map((item, index) => (
-              <tr key={index}>
-                <th className="number__patient">{index + 1}</th>
-                <td>{item.firstname}</td>
-                <td>{item.lastname}</td>
-                <td>{item.age}</td>
-                <td className="delete__patient">
-                  <BiSolidTrashAlt
-                    className="trashban"
-                  />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+  const [patient, Setpatient] = useState(patients);
+  const [name, SetName] = useState("");
+
+  function deletePatient(id) {
+    Setpatient(patient.filter((patient) => patient.id !== id));
+  }
+
+  let sortedData = patients.sort((a, b) => {
+    if (a.firstname > b.firstname) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  useEffect(() => {
+    if (name === "") {
+      Setpatient(sortedData);
+    } else {
+      Setpatient(
+        patients.filter(
+          (data) =>
+            data.firstname.toLowerCase().includes(name.toLowerCase()) ||
+            data.lastname.toLowerCase().includes(name.toLowerCase())
+        )
+      );
+    }
+  }, [name]);
+
+  return (
+    <div className="Patient">
+      <div className="searchbar">
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => {
+            SetName(e.target.value);
+          }}
+        />
+        <BiSearch />
       </div>
-    );
+      {patient.length ? 
+      <table>
+        <caption>Patients List</caption>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Age</th>
+            <th>Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {patient.map((item, index) => (
+            <tr key={index}>
+              <th className="number__patient">{index + 1}</th>
+              <td>{item.firstname}</td>
+              <td>{item.lastname}</td>
+              <td>{item.age}</td>
+              <td className="delete__patient">
+                <BiSolidTrashAlt
+                  onClick={() => deletePatient(item.id)}
+                  className="trashban"
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        </table>
+        : <div className="empty__patient">
+          <FaUserAlt/>
+        <h2>No Patients Found</h2>
+        </div> 
+      }
+    </div>
+  );
 }
 
 export default Patient;
--- src/static/PatientList.js
+++ src/static/PatientList.js
@@ -1,9 +1,9 @@
 export const patients = [
-  { id: 1, firstname: "Gulnoza", lastname: "Odilova", age: 20 },
-  { id: 2, firstname: "Gulnoza", lastname: "Odilova", age: 20 },
-  { id: 3, firstname: "Gulnoza", lastname: "Odilova", age: 20 },
-  { id: 4, firstname: "Gulnoza", lastname: "Odilova", age: 20 },
-  { id: 5, firstname: "Gulnoza", lastname: "Odilova", age: 20 },
-  { id: 6, firstname: "Gulnoza", lastname: "Odilova", age: 20 },
-  { id: 7, firstname: "Gulnoza", lastname: "Odilova", age: 20 },
+  { id: 1, firstname: "Gulnoza", lastname: "Tursunova", age: 20 },
+  { id: 2, firstname: "Mastura", lastname: "Odilova", age: 20 },
+  { id: 3, firstname: "Madina", lastname: "Rahimova", age: 20 },
+  { id: 4, firstname: "Aziza", lastname: "Olimova", age: 20 },
+  { id: 5, firstname: "Diyora", lastname: "Hakimova", age: 20 },
+  { id: 6, firstname: "Sabina", lastname: "Ismoilova", age: 20 },
+  { id: 7, firstname: "Komila", lastname: "Saidova", age: 20 },
 ];
