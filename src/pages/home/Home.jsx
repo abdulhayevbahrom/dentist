09b4e31ab0b9bc5c31cebd61efd77505d34d9677
abdulhayevbahrom <bahromjonabdulhayev2002@gmail.com>
2023-11-02 16:36:17 +0500

    fixed errors

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Perfect from '../../components/perfect/Perfect'
 import Services from '../../components/services/Services'
-import BookDentiCare from '../../components/bookDenticare/BookDentiCare'
+import BookDentiCare from '../../components/bookDentiCare/BookDentiCare'
 import Location from '../../components/contact/Location'
 import DoctorInfo from '../../components/doctors/Doctor_info'
 import Carousel from '../../components/carousel/Carousel'
@@ -9,7 +9,7 @@ import Carousel from '../../components/carousel/Carousel'
 function Home() {
     return (
         <div className='home'>
-            <Carousel/>
+            <Carousel />
             <Perfect />
             <Services />
             <DoctorInfo />
