71dfe3008474d5dc893a6c2c16b0c56d0ec9d259
fozilhon <fozilxonabdusalamov2006@gmail.com>
2023-11-02 15:40:55 +0500

    fghjkl;

--- src/admin/doctor/Doctor.jsx
+++ src/admin/doctor/Doctor.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
 import './Doctor.css'
+import { BsFacebook, BsInstagram, BsLinkedin } from "react-icons/bs"
+import { useState } from 'react';
+
+import { doctorpage } from '../../static/DoctorPage'
 
 function Doctor() {
+
+    const [openSidebar, setOpenSidebar] = useState(false);
+    openSidebar ? document.body.style.overflow = "hidden" : document.body.style.overflow = "auto"
+
     return (
         <div className='doctor'>
-            <h1>Doctor page</h1>
-        </div>
+            <div className='doctor_top'>
+                Doctor Page
+            </div>
+
+            {doctorpage.map((item, index) => (
+                openSidebar &&
+                <div key={index} className='doctor_info' onClick={() => setOpenSidebar(false)}>
+                    <div className='doctor_info_card'>
+                        <p>{item.info}</p>
+                    </div>
+                </div>
+
+
+            ))}
+            <div className='doctor_item'>
+
+                {doctorpage.map((item, index) => (
+                    <div className='doctor_items_' key={index}>
+                        <img src={item.img} alt={item.fullname} />
+                        <h2> {item.doctor}</h2>
+                        <h1>{item.fullname}</h1>
+                        <p>{item.work}</p>
+                        <div><BsFacebook />  <BsInstagram /> <BsLinkedin /></div>
+                        <a href="#" onClick={() => setOpenSidebar(true)}>info</a>
+                    </div>
+                ))
+                }
+
+
+
+            </div>
+
+        </div >
     )
 }
 
---
+++ src/static/DoctorPage.js
@@ -0,0 +1,63 @@
+export const doctorpage = [
+    {
+        doctor: "DOCTOR",
+        fullname: "Karimov A.",
+        work: "Dentist",
+        img: "https://cdn.create.vista.com/api/media/small/245268460/stock-photo-selective-focus-dentist-mask-examining-female-patient",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione assumenda rem officia unde quam dolor incidunt quis, numquam rerum voluptates, explicabo consequuntur tempora consectetur nesciunt eaque consequatur soluta omnis deserunt."
+    },
+    {
+        doctor: "DOCTOR",
+        fullname: "Karimov A.",
+        work: "Dentist",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIkrREiCKZyLSb-YBwbSps-tD0Xy2RojztCVI-T8l0JBCX4jvzsMhR1UrzVpTc5dRe8Vg&usqp=CAU",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione assumenda rem officia unde quam dolor incidunt quis, numquam rerum voluptates, explicabo consequuntur tempora consectetur nesciunt eaque consequatur soluta omnis deserunt."
+    },
+    {
+        doctor: "DOCTOR",
+        fullname: "Karimov A.",
+        work: "Dentist",
+        img: "https://bloomingtonmoderndentistry.com/wp-content/uploads/2022/07/Signs-Good-Dentist.jpg",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione assumenda rem officia unde quam dolor incidunt quis, numquam rerum voluptates, explicabo consequuntur tempora consectetur nesciunt eaque consequatur soluta omnis deserunt."
+    },
+    {
+        doctor: "DOCTOR",
+        fullname: "Karimov A.",
+        work: "Dentist",
+        img: "https://myserenitydental.com/wp-content/uploads/2022/09/Whats-the-Difference-Between-a-Dentist-and-a-Hygienist-1080x675.jpg",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione assumenda rem officia unde quam dolor incidunt quis, numquam rerum voluptates, explicabo consequuntur tempora consectetur nesciunt eaque consequatur soluta omnis deserunt."
+    }
+    ,
+    {
+        doctor: "DOCTOR",
+        fullname: "Karimov A.",
+        work: "Dentist",
+        img: "https://assets.markallengroup.com/article-images/251001/Patients%20%E2%80%9Cshocked%E2%80%9D%20as%20practice%20closes%20-%20dental%20nursing.jpg?width=800&height=533&scale=canvas",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione assumenda rem officia unde quam dolor incidunt quis, numquam rerum voluptates, explicabo consequuntur tempora consectetur nesciunt eaque consequatur soluta omnis deserunt."
+    }
+    ,
+    {
+        doctor: "DOCTOR",
+        fullname: "Karimov A.",
+        work: "Dentist",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlijdFer2eIHaDMh2y6lKbL2TaKl09RFJ8i5im7ymi-8SwLgC1SJvxgRTNPXYDFx_A5_U&usqp=CAU",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione assumenda rem officia unde quam dolor incidunt quis, numquam rerum voluptates, explicabo consequuntur tempora consectetur nesciunt eaque consequatur soluta omnis deserunt."
+    }
+    ,
+    {
+        doctor: "DOCTOR",
+        fullname: "Karimov A.",
+        work: "Dentist",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSauFHZ39gGIBz3mL6r7l_r2DSHsZlxNQrA1i4cylRsPUre86XtpSt-FbnIpz-QTnB3E6w&usqp=CAU",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione assumenda rem officia unde quam dolor incidunt quis, numquam rerum voluptates, explicabo consequuntur tempora consectetur nesciunt eaque consequatur soluta omnis deserunt."
+    }
+    ,
+    {
+        doctor: "DOCTOR",
+        fullname: "Karimov A.",
+        work: "Dentist",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6XkqmdS2X8hB1Z2RCmCVOSVcUD5Qb-m6Elij9I9tEnhBpEqYk53SvUKqqb4wtR35HBCo&usqp=CAU",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione assumenda rem officia unde quam dolor incidunt quis, numquam rerum voluptates, explicabo consequuntur tempora consectetur nesciunt eaque consequatur soluta omnis deserunt."
+    }
+
+]
